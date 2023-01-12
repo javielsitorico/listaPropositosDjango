75928b9d4af489fef883deb199eaf9be0d727d0b
javielsitorico <zx21student030@colegiovalledelmiro.es>
2023-01-12 12:29:56 +0100

    Crear proposito acabado

--- propositos/urls.py
+++ propositos/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from . import views
 
+app_name = 'propositos'
+
 urlpatterns = [
      path('', views.listaPropositos, name='listaPropositos'),
+     path('crear/', views.crearProposito, name='crearProposito'),
+     path('insertar/', views.insertarProposito, name='insertarProposito'),
 ]
--- propositos/views.py
+++ propositos/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.core.exceptions import ValidationError
 
 from .models import Proposito
 
-from datetime import timedelta, date
+from datetime import timedelta, date, datetime
 
 # proximosPropositos devuelve los propositos a cumplir en 30 dias
 def listaPropositos(request):
-     
      propositos = Proposito.objects.all()
      contexto = {
           'propositos': propositos,
      }
+          
+     return render(request, 'listaPropositos.html', contexto)
+
+def crearProposito(request):
+     return render(request, 'crearProposito.html')
+
+def insertarProposito(request):
+     if(request.POST['proposito'] == '' or request.POST['fechaObjetivo'] == ''):
+          return HttpResponseRedirect(reverse('propositos:crearProposito'))     
      
-     plantilla = loader.get_template('listaPropositos.html')
-     
-     return HttpResponse(plantilla.render(contexto, request))
+     try:
+          Proposito(
+               proposito = request.POST['proposito'], 
+               fechaObjetivo = request.POST['fechaObjetivo'], 
+               conseguido = False
+               ).save()
+     except(ValidationError):
+          return HttpResponseRedirect(reverse('propositos:crearProposito'))     
+     else:
+          return HttpResponseRedirect(reverse('propositos:listaPropositos'))
